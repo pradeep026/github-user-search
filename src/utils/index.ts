df2373cfffe8fc7af38dd9ae8f9a7135cd5e9604
Pradeep <kspradeepbe@gmail.com>
2022-04-05 23:39:01 +0800

    Add utils function to assert garud function

---
+++ src/utils/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Type gaurd function - Assert function to check the type of argument.
+ * Throws `TypeError` if the argument is not a function
+ *
+ * @param fn - any type
+ * @return boolean - true of type of `fn` is function else false
+ */
+
+export function assertIsFunction(fn: any): fn is Function {
+    return fn && typeof fn === `function`;
+};

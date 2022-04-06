ad9fb3fb93953e255bb7c2f6e245a6832f332637
Pradeep <kspradeepbe@gmail.com>
2022-04-06 14:51:18 +0800

    Integrate user search and result list component to home page

--- src/pages/userSearchPage/index.tsx
+++ src/pages/userSearchPage/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { GithubUserSearch } from '../../components/organisms/GithubUserSearch';
+import './style.scss';
 
 export const UserSearchPage: React.FC = () => {
     return (
-        <div>
-            <h1> Search Page </h1>
+        <div className='home__page'>
+            <h1 className='page__title'>Github User Search</h1>
+            <GithubUserSearch />
         </div>
     );
 };
